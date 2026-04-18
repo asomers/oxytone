e3964acdad26e8507fd26cb41d22dcbc162a3576
Alan Somers <asomers@gmail.com>
2026-04-18 17:44:36 -0600

    Add a useful comment to the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite'
 import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-	plugins: [sveltekit(), Icons({ compiler: 'svelte', scale: 1 }), tailwindcss()]
+	plugins: [sveltekit(), Icons({ compiler: 'svelte', scale: 1 }), tailwindcss()],
+	server: {
+	    // Add the oxytone server's own hostname, and any aliases, to the below list
+	    allowedHosts: []
+	  }
 })
