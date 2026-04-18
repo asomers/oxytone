asomers/oxytone

commit 963c88ae9b13bbccb107c48c25ec50b25863e540

Untrack XML files

.gitignore
@@ -28,4 +28,7 @@ data/
 .basex
 .basexgui
 
-lib/custom
+treebanks/
+lsj/
+lit/
+glaux/

commit 29ed98725b8f8b13ba02c3703f3082d0ef1bcfe9

Add a submodule for glaux
The vendored copy was deleted in 963c88ae9b1, which broke "just seed".

.gitignore
@@ -30,11 +30,6 @@ data/
 .basex
 .basexgui
 
-lsj/
-tei/
-eng/
-glaux/
-
 
 # Python
 .ropeproject/

.gitmodules
@@ -1,0 +1,3 @@
+[submodule "glaux"]
+	path = glaux
+	url = https://github.com/alekkeersmaekers/glaux.git

Justfile
@@ -25,7 +25,7 @@ normalized:
 [group('db')]
 glaux:
   basex -O MAXCATS=10000 -O ATTRINCLUDE=id,head,form,lemma,relation,speaker,div_chapter,div_section,analysis \
-        -c "CREATE DB glaux glaux/"
+        -c "CREATE DB glaux glaux/xml"
 
 [group('db')]
 tei: